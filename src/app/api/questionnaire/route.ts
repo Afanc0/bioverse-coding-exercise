2c5b17d2df4d63f1fe75b04ac0683930aecfa946
afanc0 <gianromani.gr@gmail.com>
2024-12-22 16:08:46 -0500

    fix(api): allow access control to all

--- src/app/api/questionnaire/route.ts
+++ src/app/api/questionnaire/route.ts
@@ -4,6 +4,9 @@ import questionnaire from "@bioverse-intake/data/questionnaire.json"
 export async function GET(_request: Request): Promise<Response> {
     return new Response(JSON.stringify(questionnaire), {
         status: 200,
-        headers: { 'Content-Type': 'application/json' }
+        headers: { 
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
     })
 }
