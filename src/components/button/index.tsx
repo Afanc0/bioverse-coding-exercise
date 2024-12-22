af2bcf54a2615d8f27022230d00aeb82cf0bb3ae
afanc0 <gianromani.gr@gmail.com>
2024-12-22 16:10:41 -0500

    refactor(button): added action to button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,13 +1,15 @@
 
 interface ButtonProps {
     text: string;
+    onClick?: () => void;
 }
 
 const Button: React.FC<ButtonProps> = ({
-    text
+    text,
+    onClick
 }) => {
     return (
-        <button className="bg-[#286ba2] flex-1 rounded-full py-4 hover:bg-[#3b8fd6]">
+        <button className="bg-[#286ba2] flex-1 rounded-full py-4 hover:bg-[#3b8fd6]" onClick={onClick}>
             <span className="text-white text-base font-semibold">{text}</span>
         </button>
     )
