7def0e2fedb3b5c2b7d23b3f6b4913f36e694ba8
afanc0 <gianromani.gr@gmail.com>
2024-12-22 16:11:37 -0500

    feat(hook): created get question by id hook

---
+++ src/hooks/Questions/index.ts
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface QuestionItem {
+  _id: number;
+  type: string;
+  options?: string[];
+  question: string;
+}
+
+export const useGetQuestionsById = (id: string) => {
+  const [data, setData] = React.useState<QuestionItem[] | null>(null);
+  const [error, setError] = React.useState<string | null>(null);
+  const [loading, setLoading] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://192.168.1.146:3000/api/question?question=${id}`, {
+          method: 'GET',
+        })
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const result: QuestionItem[] = await response.json()
+        setData(result);
+      } catch (error: any) {
+        setError(error.message || 'An unknown error occurred')
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData()
+  }, [id])
+
+  return { data, error, loading }
+};
